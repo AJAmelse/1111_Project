1290900e675656a8552e52aa0de9af5342325d92
AJAmelse <artemis.amelse@hopkinsrobotics.org>
2026-03-25 10:38:29 -0500

    swap to using enums

--- main.c
+++ main.c
@@ -8,195 +8,165 @@
 #include <time.h>
 int main(){
     //store player location as 3x3 grid of booleans
-    int location[3][3] = {{0,0,0},{0,1,0},{0,0,0}};
+    enum location {downpour, mountain, lava, snow, forest, lake, river, cave, button};
+    enum location current = forest;
     int running = 1;
     char input = 'q';
     //set rand seed to unix timestamp to have variable output from program
     srand(time(NULL));
 
     while(running){
-        //check through all values of 2d location array
-        for(int i=0; i < 3; i++){
-            for(int j=0; j<3; j++){
-                //check if current value is set to true
-                if(location[i][j]){
-                    //different output based on location
-                    if(i == 0 && j == 0){
-                        printf("There is a torrential downpour and lightning crackles in the sky\n");
-                        printf("move right(d), or down(s), or quit(q): ");
-                        scanf(" %c", &input);
-                        //change position in array
-                        if(input == 's'){
-                            location[i][j] = 0;
-                            location[i+1][j] = 1;
-                        }else if(input == 'd'){
-                            location[i][j] = 0;
-                            location[i][j+1] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
-                    if(i == 1 && j == 0){
-                        printf("You come up to the side of a mountain \n");
-                        printf("move up(w), right(d), or down(s), or quit(q): ");
-                        scanf(" %c", &input);
-                        if(input == 's'){
-                            location[i][j] = 0;
-                            location[i+1][j] = 1;
-                        }else if(input == 'd'){
-                            location[i][j] = 0;
-                            location[i][j+1] = 1;
-                        }else if(input == 'w'){
-                            location[i][j] = 0;
-                            location[i-1][j] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
-                    if(i == 2 && j == 0){
-                        printf("A lava stream blocks off your path \n");
-                        printf("move right(d), or up(w), or quit(q): ");
-                        scanf(" %c", &input);
-                        if(input == 'w'){
-                            location[i][j] = 0;
-                            location[i-1][j] = 1;
-                        }else if(input == 'd'){
-                            location[i][j] = 0;
-                            location[i][j+1] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
-                    if(i == 0 && j == 1){
-                        printf("It suddenly got cold and there is a foot of snow on the ground\n");
-                        printf("move left(a), right(d), or down(s), or quit(q): ");
-                        scanf(" %c", &input);
-                        if(input == 's'){
-                            location[i][j] = 0;
-                            location[i-1][j] = 1;
-                        }else if(input == 'a'){
-                            location[i][j] = 0;
-                            location[i][j-1] = 1;
-                        }else if(input == 'd'){
-                            location[i][j] = 0;
-                            location[i][j+1] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
-                    if(i == 1 && j == 1){
-                        printf("You are standing in the center of a forest path splitting in 4 directions\n");
-                        printf("move up(w), left(a), right(d), or down(s): ");
-                        scanf(" %c", &input);
-                        if(input == 'w'){
-                            location[i][j] = 0;
-                            location[i-1][j] = 1;
-                        }else if(input == 's'){
-                            location[i][j] = 0;
-                            location[i+1][j] = 1;
-                        }else if(input == 'a'){
-                            location[i][j] = 0;
-                            location[i][j-1] = 1;
-                        }else if(input == 'd'){
-                            location[i][j] = 0;
-                            location[i][j+1] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
-                    if(i == 2 && j == 1){
-                        printf("A massive lake spreads out in front of you\n");
-                        printf("move left(a), right(d), or up(w), or quit(q): ");
-                        scanf(" %c", &input);
-                        if(input == 'w'){
-                            location[i][j] = 0;
-                            location[i-1][j] = 1;
-                        }else if(input == 'a'){
-                            location[i][j] = 0;
-                            location[i][j-1] = 1;
-                        }else if(input == 'd'){
-                            location[i][j] = 0;
-                            location[i][j+1] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
-                    if(i == 0 && j == 2){
-                        printf("A river blocks off your path\n");
-                        printf("move left(a), or down(s), or quit(q): ");
-                        scanf(" %c", &input);
-                        if(input == 's'){
-                            location[i][j] = 0;
-                            location[i+1][j] = 1;
-                        }else if(input == 'a'){
-                            location[i][j] = 0;
-                            location[i][j-1] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
-                    if(i == 1 && j == 2){
-                        printf("The path goes through a dark cave\n");
-                        printf("move left(a), up(w), or down(s), or quit(q): ");
-                        scanf(" %c", &input);
-                        if(input == 's'){
-                            location[i][j] = 0;
-                            location[i+1][j] = 1;
-                        }else if(input == 'a'){
-                            location[i][j] = 0;
-                            location[i][j-1] = 1;
-                        }else if(input == 'w'){
-                            location[i][j] = 0;
-                            location[i-1][j] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
-                    if(i == 2 && j == 2){
-                        printf("The path comes to a corner, with a button on a pedestal\n");
-                        printf("move left(a), or up(w), or pess the button(e), or quit(q): ");
-                        scanf(" %c", &input);
-                        if(input == 'w'){
-                            location[i][j] = 0;
-                            location[i-1][j] = 1;
-                        }else if(input == 'a'){
-                            location[i][j] = 0;
-                            location[i][j-1] = 1;
-                        }else if(input == 'q'){
-                            running = 0;
-                        }else if(input == 'e'){
-                            int num = rand() % 101;
-                            if(num > 75){
-                                printf("Confetti shoots into the sky\n");
-                            }else if(num >50){
-                                printf("Lightning strikes a tree next to you\n");
-                            }else if(num > 25){
-                                printf("The Northern Lights appear in the sky\n");
-                            }else{
-                                printf("Nothing happens\n");
-                            }
-                        }else{
-                            printf("invalid input\n");
-                        }
-                    }
+        switch (current) {
+            case downpour:
+                printf("There is a torrential downpour and lightning crackles in the sky\n");
+                printf("move right(d), or down(s), or quit(q): ");
+                scanf(" %c", &input);
+                //change position in array
+                if(input == 's'){
+                    current = mountain;
+                }else if(input == 'd'){
+                    current = snow;
+                }else if(input == 'q'){
+                    running = 0;
+                }else{
+                    printf("invalid input\n");
                 }
-            }
+ 
+                break;
+            case mountain:
+                printf("You come up to the side of a mountain \n");
+                printf("move up(w), right(d), or down(s), or quit(q): ");
+                scanf(" %c", &input);
+                if(input == 's'){
+                    current = lava;
+                }else if(input == 'd'){
+                    current = forest;
+                }else if(input == 'w'){
+                    current = downpour;
+                }else if(input == 'q'){
+                    running = 0;
+                }else{
+                    printf("invalid input\n");
+                }
+                break;
+            case lava:
+                printf("A lava stream blocks off your path \n");
+                printf("move right(d), or up(w), or quit(q): ");
+                scanf(" %c", &input);
+                if(input == 'w'){
+                    current = mountain;
+                }else if(input == 'd'){
+                    current = lake;
+                }else if(input == 'q'){
+                running = 0;
+                }else{
+                    printf("invalid input\n");
+                }
+                break;
+            case snow:
+                printf("It suddenly got cold and there is a foot of snow on the ground\n");
+                printf("move left(a), right(d), or down(s), or quit(q): ");
+                scanf(" %c", &input);
+                if(input == 's'){
+                    current = forest;
+                }else if(input == 'a'){
+                    current = downpour;
+                }else if(input == 'd'){
+                    current = river;
+                }else if(input == 'q'){
+                    running = 0;
+                }else{
+                    printf("invalid input\n");
+                }
+                break;
+            case forest:
+                printf("You are standing in the center of a forest path splitting in 4 directions\n");
+                printf("move up(w), left(a), right(d), or down(s): ");
+                scanf(" %c", &input);
+                if(input == 'w'){
+                    current = snow;
+                }else if(input == 's'){
+                    current = lake;
+                }else if(input == 'a'){
+                    current = mountain;
+                }else if(input == 'd'){
+                    current = cave;
+                }else if(input == 'q'){
+                    running = 0;
+                }else{
+                    printf("invalid input\n");
+                }
+            case lake:
+                printf("A massive lake spreads out in front of you\n");
+                printf("move left(a), right(d), or up(w), or quit(q): ");
+                scanf(" %c", &input);
+                if(input == 'w'){
+                    current = forest;
+                }else if(input == 'a'){
+                    current = lava;
+                }else if(input == 'd'){
+                    current = button;
+                }else if(input == 'q'){
+                    running = 0;
+                }else{
+                    printf("invalid input\n");
+                }
+                break;
+            case river:
+                printf("A river blocks off your path\n");
+                printf("move left(a), or down(s), or quit(q): ");
+                scanf(" %c", &input);
+                if(input == 's'){
+                    current = cave;
+                }else if(input == 'a'){
+                    current = snow;
+                }else if(input == 'q'){
+                    running = 0;
+                }else{
+                    printf("invalid input\n");
+                }
+                break;
+            case cave:
+                printf("The path goes through a dark cave\n");
+                printf("move left(a), up(w), or down(s), or quit(q): ");
+                scanf(" %c", &input);
+                if(input == 's'){
+                    current = button;
+                }else if(input == 'a'){
+                    current = forest;
+                }else if(input == 'w'){
+                    current = river;
+                }else if(input == 'q'){
+                    running = 0;
+                }else{
+                    printf("invalid input\n");
+                }
+                break;
+            case button:
+                printf("The path comes to a corner, with a button on a pedestal\n");
+                printf("move left(a), or up(w), or pess the button(e), or quit(q): ");
+                scanf(" %c", &input);
+                if(input == 'w'){
+                    current = cave;
+                }else if(input == 'a'){
+                    current = lake;
+                }else if(input == 'q'){
+                    running = 0;
+                }else if(input == 'e'){
+                    int num = rand() % 101;
+                    if(num > 75){
+                        printf("Confetti shoots into the sky\n");
+                    }else if(num >50){
+                        printf("Lightning strikes a tree next to you\n");
+                    }else if(num > 25){
+                        printf("The Northern Lights appear in the sky\n");
+                    }else{
+                        printf("Nothing happens\n");
+                    }
+                }else{
+                    printf("invalid input\n");
+                }
+                break;
         }
     }
     return 0;
